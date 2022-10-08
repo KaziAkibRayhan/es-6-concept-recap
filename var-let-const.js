80f6d0cb884278ad9370e7495542413245cc0254
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-08 17:13:40 +0600

    var let const

---
+++ var-let-const.js
@@ -0,0 +1,16 @@
+// var
+// var myName = 'Akib';
+// myName = 'Rayhan'; // reassign available
+// var myName = 'Akib Rayhan' // redefine available
+
+// let
+// let myName = 'Akib';
+// myName = 'Rayhan'; // reassign available
+// var myName = 'Akib Rayhan' // redefine not available
+
+// const
+// const myName = 'Akib';
+// myName = 'Rayhan'; // reassign not available
+// var myName = 'Akib Rayhan' // redefine not available
+
+// console.log(myName);
