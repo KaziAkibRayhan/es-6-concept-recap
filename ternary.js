8fce74c5c6f788d5d049a548f0fd33fd21bd5bb8
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-08 21:13:31 +0600

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,11 @@
+const isHotDay = true;
+
+if (isHotDay === true) {
+    console.log('today is hot');
+}
+else{
+    console.log('today is not hot');
+}
+
+const today = (isHotDay === true) ? 'today is hot': 'today is not hot';
+console.log(today);
