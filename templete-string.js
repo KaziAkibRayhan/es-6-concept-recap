0fb3f7187d54102a0bcfdd112d6f1947340aef7c
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-08 18:43:10 +0600

    templete string added

---
+++ templete-string.js
@@ -0,0 +1,10 @@
+const myName = 'Akib';
+const age = 23;
+const job = 'Web Developer';
+
+
+const sentence = `My name is : ${myName}.
+I am ${age} years old.
+I am a ${job}.
+`;
+console.log(sentence);
