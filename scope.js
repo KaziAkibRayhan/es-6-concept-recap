37e6ca70e1f548576352b97ff4d1987190af705f
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-08 17:38:38 +0600

    scope practice

--- scope.js
+++ scope.js
@@ -1,4 +1,20 @@
 // global scope
-// local scope
 // block scope
+// local scope
 
+// const a = 39;
+// console.log(a); // global scope
+
+// {
+//     const b = 23;
+//     console.log('block scope ',b); // block scope
+// }
+// console.log(b) ; // block not available
+
+function add() {
+    var result = 6 + 7; // local scope
+    console.log('inside function', result);
+}
+add();
+
+console.log(result);
